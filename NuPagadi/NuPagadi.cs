1335de37b42697d6971f36f620ac6bb46c8772ec
tamasii <milkokochev@mail.bg>
2015-02-12 21:18:00 +0200

    Metodi - skelet

---
+++ NuPagadi/NuPagadi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class NuPagadi
+    {
+        static void Main()
+        {
+            // GAME START SCREEN
+            //Draw the field - from txt file
+            //Draw the eggs
+            //Move the eggs
+            //Move the wolf
+            //Scoring system
+
+
+        }
+    }
